af05cd0debdb35e21f053c685509fd9559471749
S3rpa <sergiobarbera1@gmail.com>
2024-08-26 03:17:22 +0200

    updating db and user page

--- app/store.js
+++ app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookingsReducer from '../features/bookings/bookingsSlice';
+import usersReducer from '../features/users/usersSlice';
 
 export const store = configureStore({
     reducer: {
         bookings: bookingsReducer,
+        users: usersReducer
     },
 });
 
---
+++ features/users/usersThunk.js
@@ -0,0 +1,91 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const baseURL = 'https://my-json-server.typicode.com/S3rpa/HotelMiranda-Dashboard';
+
+export const GetUsers = createAsyncThunk(
+    'users/getUsers',
+    async () => {
+        const response = await fetch(`${baseURL}/users`)
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error('Could not reach server: ' + response.status);
+                }
+                return response.json();
+            })
+            .catch((error) => {
+                throw new Error(error);
+            });
+        return response;
+    }
+);
+
+export const EditUser = createAsyncThunk(
+    'users/editUser',
+    async (updatedUser) => {
+        const response = await fetch(`${baseURL}/users/${updatedUser.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+            dataType: 'json',
+            body: JSON.stringify(updatedUser),
+        })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error('Could not reach server: ' + response.status);
+                }
+                return response.json();
+            })
+            .catch((error) => {
+                throw new Error(error);
+            });
+        return response;
+    }
+);
+
+export const DeleteUser = createAsyncThunk(
+    'users/deleteUser',
+    async (userId) => {
+        const response = await fetch(`${baseURL}/users/${userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+            dataType: 'json',
+        })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error('Could not reach server: ' + response.status);
+                }
+                return userId;
+            })
+            .catch((error) => {
+                throw new Error(error);
+            });
+        return response;
+    }
+);
+
+export const CreateUser = createAsyncThunk(
+    'users/createUser',
+    async (newUser) => {
+        const response = await fetch(`${baseURL}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+            dataType: 'json',
+            body: JSON.stringify(newUser),
+        });
+
+        if (response.status >= 400) {
+            throw new Error('Could not reach server: ' + response.status);
+        }
+
+        const createdUser = await response.json();
+        return createdUser;
+    }
+);
